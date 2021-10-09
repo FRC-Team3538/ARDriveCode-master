9fc11603860368838ab1c0821fc16dd5a228e178
LBJANU <59941227+LBJANU@users.noreply.github.com>
2021-10-09 11:31:46 -0400

    Autonomous Program Refined

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -15,6 +15,11 @@ using namespace frc;
 class Robot : public frc::TimedRobot
 {
 public:
+  void RobotInit() override
+  {
+    m_drive.ResetEncoders();
+  }
+
   void RobotPeriodic() override
   {
     m_drive.UpdateSmartdash();
@@ -25,51 +30,78 @@ public:
   {
     m_autoTimer.Start();
     m_drive.ResetEncoders();
-    int m_state = 0;
+    m_state = 0;
   }
 
   void AutonomousPeriodic() override
   {
     switch (m_state)
     {
+
     case 0:
     {
-      m_state++;
-    }
-    break;
-  case 1:
-  {
-    m_drive.Drive(0.2, 0.0);
-    if (m_drive.GetEncoderPositionLeft() > 113.0)
-    {
-      m_state++;
-    }
-  }
-  break;
-  case 2:
-  {
-    double fwd = 0.00;
-    double rot = -0.20;
-    m_drive.Drive(fwd, rot);
-    if (m_autoTimer.Get() > 0.30)
-    {
-      m_state++;
-    }
-  }
-  case 3:
-  {
-    m_drive.Drive(0.3, 0.0);
-    if (m_drive.GetEncoderPositionLeft() > 20.0)
-    {
-      m_state++;
-    }
-  }
-  break;
-  default:
-    m_drive.Drive(0.0, 0.0);
-  }
 
-  /* case 0:
+      m_drive.Drive(0.0, -0.1);
+      if (m_autoTimer.Get() > 1.0) //1.2
+      {
+        m_state++;
+      }
+      break;
+    }
+
+    case 1:
+    {
+      m_drive.Drive(-0.2, 0.0);
+      if (m_drive.GetEncoderPositionLeft() < -50.0)
+      {
+        m_state++;
+        m_autoTimer.Reset();
+        m_autoTimer.Start();
+        m_drive.ResetEncoders();
+      }
+      break;
+    }
+
+    case 2:
+    {
+      frc::SmartDashboard::PutNumber("Timer", m_autoTimer.Get());
+      m_drive.Drive(0.0, 0.10);
+      if (m_autoTimer.Get() > 0.5)
+      {
+        m_state++;
+        m_autoTimer.Reset();
+        m_autoTimer.Start();
+      }
+      break;
+    }
+
+    case 3:
+    {
+      m_drive.Drive(-0.3, 0.0);
+      if (m_drive.GetEncoderPositionLeft() < -25.0)
+      {
+        m_state++;
+        m_autoTimer.Reset();
+        m_autoTimer.Start();
+      }
+      break;
+    }
+
+    case 4:
+    {
+      m_drive.Drive(-0.05, 0.0);
+      m_drive.Dropper(0.0, 1.0);
+      if (m_autoTimer.Get() > 1.0)
+      {
+        m_state++;
+      }
+      break;
+    }
+    default:
+      m_drive.Drive(0.0, 0.0);
+    }
+
+    /* case 0:
     {
       double fwd = 0.35;
       double rot = 0.00;
@@ -97,67 +129,66 @@ public:
       m_drive.Drive(0.0, 0.0);
     }
   */
-}
-
-void
-TeleopPeriodic() override
-{
-  // Get the x speed. We are inverting this because Xbox controllers return
-  // negative values when we push forward.
-  auto forward = m_controller.GetY(frc::GenericHID::kLeftHand);
-
-  // Get the rate of angular rotation. We are inverting this because we want a
-  // positive value when we p\ull to the left (remember, CCW is positive in
-  // mathematics). Xbox controllers return positive values when you pull to
-  // the right by default.
-  auto rotation = m_controller.GetX(frc::GenericHID::kRightHand);
-  forward = -Deadbandforward(forward, 0.05);
-  rotation = -Deadbandforward(rotation, 0.05);
-  m_drive.Drive(forward, rotation);
-
-  auto front = m_opcontroller.GetTriggerAxis(frc::GenericHID::kLeftHand);
-  front = Deadbandforward(front, 0.05);
-  auto backward = m_opcontroller.GetTriggerAxis(frc::GenericHID::kRightHand);
-  backward = Deadbandforward(backward, 0.05);
-  m_drive.Climb(front, backward);
-
-  auto speed = m_controller.GetCircleButton();
-  auto back = m_controller.GetSquareButton();
-  m_drive.Dropper(speed, back);
-}
-
-double Deadbandforward(double forward, double deadband)
-{
-  if ((std::abs(forward)) < deadband)
-  {
-    return 0.0;
   }
-  else if (forward > 0.95)
+
+  void
+  TeleopPeriodic() override
   {
-    return 1.0;
+    // Get the x speed. We are inverting this because Xbox controllers return
+    // negative values when we push forward.
+    auto forward = m_controller.GetY(frc::GenericHID::kLeftHand);
+
+    // Get the rate of angular rotation. We are inverting this because we want a
+    // positive value when we p\ull to the left (remember, CCW is positive in
+    // mathematics). Xbox controllers return positive values when you pull to
+    // the right by default.
+    auto rotation = m_controller.GetX(frc::GenericHID::kRightHand);
+    forward = -Deadbandforward(forward, 0.05);
+    rotation = -Deadbandforward(rotation, 0.05);
+    m_drive.Drive(forward, rotation);
+
+    auto front = m_controller.GetTriggerAxis(frc::GenericHID::kLeftHand);
+    front = Deadbandforward(front, 0.05);
+    auto backward = m_controller.GetTriggerAxis(frc::GenericHID::kRightHand);
+    backward = Deadbandforward(backward, 0.05);
+    m_drive.Climb(front, backward);
+
+    auto speed = m_controller.GetCircleButton();
+    auto back = m_controller.GetSquareButton();
+    m_drive.Dropper(speed, back);
   }
-  else if (forward < -0.95)
+
+  double Deadbandforward(double forward, double deadband)
   {
-    return -1.0;
+    if ((std::abs(forward)) < deadband)
+    {
+      return 0.0;
+    }
+    else if (forward > 0.95)
+    {
+      return 1.0;
+    }
+    else if (forward < -0.95)
+    {
+      return -1.0;
+    }
+    else
+    {
+      return forward;
+    }
   }
-  else
-  {
-    return forward;
-  }
-}
 
 private:
-PS4Controller m_controller{0};
-PS4Controller m_opcontroller{1};
-// Slew rate limiters to make joystick inputs more gentle; 1/3 sec from 0
-// to 1.
-frc::SlewRateLimiter<units::scalar> m_speedLimiter{3 / 1_s};
-frc::SlewRateLimiter<units::scalar> m_rotLimiter{3 / 1_s};
-int m_state = 0;
-Timer m_autoTimer;
-Drivetrain m_drive;
-}
-;
+  PS4Controller m_controller{0};
+  PS4Controller m_opcontroller{1};
+  // Slew rate limiters to make joystick inputs more gentle; 1/3 sec from 0
+  // to 1.
+  frc::SlewRateLimiter<units::scalar> m_speedLimiter{3 / 1_s};
+  frc::SlewRateLimiter<units::scalar> m_rotLimiter{3 / 1_s};
+  int m_state = 0;
+  Timer m_autoTimer;
+  Drivetrain m_drive;
+};
 
 #ifndef RUNNING_FRC_TESTS
 int main()
